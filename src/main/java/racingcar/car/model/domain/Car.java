fc1722940096e2fd9c392e732499ddfdb1dc6699
sjh <thdwmdgns@naver.com>
2022-04-19 20:40:05 +0900

    feat(Car.java): Car 이동 로직 구현

--- src/main/java/racingcar/car/model/domain/Car.java
+++ src/main/java/racingcar/car/model/domain/Car.java
@@ -3,6 +3,7 @@ package racingcar.car.model.domain;
 public class Car {
     private static final int NAME_MAX_LENGTH = 5;
     private static final int NAME_MIN_LENGTH = 1;
+    private final int CAR_MOVE_CONDITION = 4;
     private int location;
     private String name;
 
@@ -20,5 +21,14 @@ public class Car {
     private static void checkCarNameLength(String name) {
         if(name.length()>NAME_MAX_LENGTH || name.length()<NAME_MIN_LENGTH)
             throw new IllegalArgumentException("차 이름은 5글자를 넘을수 없습니다");
+    }
+
+    public int move(int input) {
+        location += getIntFromBoolean(input>=CAR_MOVE_CONDITION);
+        return location;
+    }
+
+    private int getIntFromBoolean(boolean flag) {/* 반환값 : 1(flag = true), 0(flag = false) */
+        return 1 & Boolean.hashCode(flag) >> 1;
     }
 }
