fb7f960d75295972ccfb08d996ad3490e7296ba0
sjh <thdwmdgns@naver.com>
2022-04-19 20:39:31 +0900

    test(CarTest.java): Car 이동 로직 테스트

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,7 +2,12 @@ package racingcar.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.car.model.domain.Car;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -24,5 +29,31 @@ public class CarTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> Car.createCar(""))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"4:1","5:1","6:1","7:1","8:1","9:1","0:0","1:0","2:0","3:0"},delimiter = ':')
+    public void Car이동시키기(int moveForRandomInput,int locationResult) {
+        //given
+        int t1Location = t1.move(moveForRandomInput);
+        int t2Location = t2.move(moveForRandomInput);
+        int t3Location = t3.move(moveForRandomInput);
+        //when
+        assertThat(t1Location).isEqualTo(locationResult);
+        assertThat(t2Location).isEqualTo(locationResult);
+        assertThat(t3Location).isEqualTo(locationResult);
+        //then
+    }
+
+    @Test
+    public void Car연속이동시키기 (){
+        //given
+        List<Integer> inputs = Arrays.asList(1, 2, 4, 7, 3, 9, 8);
+        List<Integer> positions = Arrays.asList(0,0,1,2,2,3,4);
+
+        //then
+        for(int i=0;i<inputs.size();i++){
+            assertThat(t1.move(inputs.get(i))).isEqualTo(positions.get(i));
+        }
     }
 }
