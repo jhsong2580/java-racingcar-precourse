81a8db5c57a7a5e1e0af183d5ac420a5a52bb36f
sjh <thdwmdgns@naver.com>
2022-04-19 23:48:30 +0900

    test(Cars.java): Cars를통한 move 호출 로직 테스트

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -1,14 +1,35 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.Application;
+import racingcar.car.model.domain.Car;
 import racingcar.car.model.domain.Cars;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class CarsTest {
+public class CarsTest extends NsTest {
+    private Cars cars;
+    @BeforeEach
+    public void initCarsTest(){
+        cars = Cars.createCars("c1,c2,c3");
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"t1,t2,t3,t4,t5","t,t,t,t,t","ttttt,tttt,tttt,tttt,tttt"})
     public void 문자열로부터_차생성하기_정상동작 (String input){
@@ -20,6 +41,36 @@ public class CarsTest {
     public void 문자열로부터_차생성하기_실패(String input){
         assertThatThrownBy(() -> Cars.createCars(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void Cars를통한CarMove호출(){
+        assertRandomNumberInRangeTest(
+                ()->{
+                    LinkedHashMap<String, Integer> move = null;
+                    for(int i=0;i<9;i++) {
+                         move = cars.move();
+                    }
+                    assertThat(move).containsExactly(
+                            entry("c1",5),
+                            entry("c2",5),
+                            entry("c3",6)
+                    );
+
+                },
+            3,8,5,
+                5,5,6,
+                2,1,2,
+                1,7,5,
+                9,9,4,
+                6,2,1,
+                7,0,2,
+                4,2,8,
+                2,4,7
+        );
+
+
+
 
     }
 }
