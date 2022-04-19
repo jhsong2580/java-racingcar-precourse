76856bc40d631c12c10aa1f9af4ab52b019673a1
sjh <thdwmdgns@naver.com>
2022-04-19 23:13:46 +0900

    feat(Cars.java): Cars 일급콜렉션 생성 로직 구현

---
+++ src/main/java/racingcar/car/model/domain/Cars.java
@@ -0,0 +1,32 @@
+package racingcar.car.model.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    protected Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars createCars(String input) {
+        String[] names = validateSplitResultSizeMoreThanZero(input);
+        return new Cars(createCarsByInput(names));
+    }
+
+    private static List<Car> createCarsByInput(String[] names) {
+        ArrayList<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(Car.createCar(name));
+        }
+        return cars;
+    }
+
+    private static String[] validateSplitResultSizeMoreThanZero(String input) {
+        String[] names = input.trim().split(",");
+        if(names.length==0)
+            throw new IllegalArgumentException("최소 한개 이상의 차 이름이 있어야합니다");
+        return names;
+    }
+}
