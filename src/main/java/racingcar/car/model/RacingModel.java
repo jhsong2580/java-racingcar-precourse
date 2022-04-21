fe4f1d0cf13ceacd43fe35ffc2d8f5d2e3370e8a
sjh <thdwmdgns@naver.com>
2022-04-21 17:01:27 +0900

    feat: model 내 domain 기능 연결
    Model 내 createCars, move, electWinner 기능 생성

--- src/main/java/racingcar/car/model/RacingModel.java
+++ src/main/java/racingcar/car/model/RacingModel.java
@@ -1,16 +1,18 @@
 package racingcar.car.model;
 
 import racingcar.car.GameStatus;
+import racingcar.car.model.domain.Cars;
 import racingcar.car.model.validator.*;
 import racingcar.car.view.RacingView;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class RacingModel {
     private final List<Validator> validators = new ArrayList<>();
     private final RacingView racingView;
-
+    private Cars cars;
     protected RacingModel() {
         getValidators();
         racingView = RacingView.createRacingView();
@@ -41,5 +43,17 @@ public class RacingModel {
         if (validator.canValidate(gameStatus) == false)
             return true;
         return validator.validate(input);
+    }
+
+    public void createCars(String input) {
+        cars = Cars.createCars(input);
+    }
+
+    public LinkedHashMap<String, Integer> move(){
+        return cars.move();
+    }
+
+    public ArrayList<String> electWinner(){
+         return cars.electWinner();
     }
 }
